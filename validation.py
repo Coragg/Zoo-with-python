7bf8592717a9385a20e16096cf9613d805fa2dd6
Dalark <v_e_c_g@outlook.com>
2023-07-02 21:21:25 -0400

    creación de los archivos .py para las clases de los animales

--- validation.py
+++ validation.py
@@ -1,8 +1,10 @@
-def input_natural_number(message_to_user: str):
+def input_number(message_to_user: str):
     while True:
         try:
             number = eval(input(message_to_user))
-            if number >= 0.1:
+            if number > 0:
                 return number
         except ValueError:
             print("Recuerde que tiene que digitar un numero.")
+
+
